e3f6a8348a06d3b0f7698a18f740daac026c3908
naimulhuda-official <developernaimulhuda@gmail.com>
2023-12-13 21:44:31 +0600

    first commit

---
+++ index.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Module one</title>
+</head>
+<body>
+    <h2>This is naimul huda Who would like to learn Development </h2>
+    <?php
+    echo"<u>This is Naimul huda Who would like to learn Develolpment</u>";
+
+    ?>
+    
+
+    <?php
+    $teachername="Naimul huda";
+    $coursename="Web development";
+    ?>
+
+  <h1>Teacher Name is <?php echo $teachername;?></h1>
+  <h1>Course name is <?php echo $coursename?></h1>
+
+  <?php 
+  $mothername="Kohinur Begum";
+  $fathername="Bodiuz Zaman";
+  $bothername="Nazmul huda";
+  $sistername="Momotaz Begum";
+  $onlytherebelovedson="Naimul huda";
+
+  ?>
+<h2>My mother name is <?php echo $mothername;?></h2>
+<h2>My Father name is <?php echo $fathername;?></h2>
+<h2>Ny brother name is <?php echo $bothername;?></h2>
+<h2>My sister name is <?php echo $sistername;?></h2>
+<h2>Only there beloved son is <?php echo $onlytherebelovedson?></h2>
+
+
+
+
+
+
+
+
+</body>
+</html>
